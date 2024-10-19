f8709a686a6712478cd79af9937f14ebb230e52f
Getulio <augustoge71@gmail.com>
2024-10-19 20:44:08 -0300

    Configurando o botão Categoria

---
+++ EstoqueWEB/Pages/Forms/CategoriaForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EstoqueWEB.Pages.Forms
+{
+    public class CategoriaFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
